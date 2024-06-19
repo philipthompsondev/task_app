f88660ef5e56e5c8d308116cca80659222d00682
Philip Thompson <philip.thompson.dev@gmail.com>
2024-06-19 15:43:52 -0400

    removed dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    return redirect('/tasks');
 });
 
 Route::resource('tasks', TaskController::class)
