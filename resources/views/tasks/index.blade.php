96b6e62d95f6a551d99d968b81fe69d1b136f0ad
Philip Thompson <philip.thompson.dev@gmail.com>
2024-06-20 13:35:46 -0400

    Moved task to componenet

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -13,37 +13,9 @@
             <x-primary-button class="mt-4">{{ __('New Task') }}</x-primary-button>
         </form>
 
-        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+        <div class="mt-6 space-y-2">
             @foreach ($tasks as $task)
-                <div class="p-6 flex space-x-2">
-                    <div class="flex-1">
-                        <p class="">{{ $task->task_name }}</p>
-                    </div>
-
-                    <x-dropdown>
-                        <x-slot name="trigger">
-                            <button>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                </svg>
-                            </button>
-                        </x-slot>
-
-                        <x-slot name="content">
-                            <x-dropdown-link :href="route('tasks.edit', $task)">
-                                {{ __('Edit') }}
-                            </x-dropdown-link>
-                            <form method="POST" action="{{ route('tasks.destroy', $task) }}">
-                                @csrf
-                                @method('delete')
-
-                                <x-dropdown-link :href="route('tasks.destroy', $task)" onclick="event.preventDefault(); this.closest('form').submit();">
-                                    {{ __('Delete') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
-                </div>
+                <x-tasks.task :task="$task"></x-tasks.task>
             @endforeach
         </div>
     </div>
