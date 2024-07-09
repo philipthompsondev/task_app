2edc794e5cc05b721765e58533377556cb561c11
Philip Thompson <philip.thompson.dev@gmail.com>
2024-07-09 14:23:13 -0400

    Task tests

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'priority' => fake()->unique()->randomDigit(),
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+    }
+}
---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testTaskIndex(): void
+    {
+        $response = $this
+            ->get('/tasks');
+
+        $response
+            ->assertStatus(200)
+            ->assertOk();
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function testTaskStore(): void
+    {
+        $response = $this
+            ->post('/tasks', [
+                'name' => 'Test',
+                'priority' => 1
+            ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/tasks');
+
+        $task = Task::latest()->first();
+
+        $this->assertSame('Test', $task->name);
+        $this->assertSame(1, $task->priority);
+    }
+
+    public function testTaskEdit()
+    {
+        $task = Task::factory()->create();
+
+        $response = $this
+            ->get('/tasks/'.$task->id.'/edit', [
+                $task
+            ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertOk();
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function testTaskUpdate(): void
+    {
+        $task = Task::factory()->create();
+
+        $response = $this
+            ->patch('/tasks/'.$task->id, [
+                'name' => 'Edit Test',
+            ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/tasks');
+
+        $editedTask = Task::latest()->first();
+
+        $this->assertSame('Edit Test', $editedTask->name);
+    }
+
+    public function testTaskDestroy()
+    {
+        $task = Task::factory()->create();
+
+        $response = $this
+            ->delete('/tasks/'.$task->id, [
+                $task
+            ]);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/tasks/');
+    }
+}
